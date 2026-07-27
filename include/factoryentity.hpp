037c5edfe5aaef8171aee5a6bfe54145dd72dab0
Arnav Karn <arnavkarna877@gmail.com>
2026-07-27 05:13:55 -0500

    added new entity state and eventtypes

--- include/factoryentity.hpp
+++ include/factoryentity.hpp
@@ -5,21 +5,24 @@
 #include<print>
 
 enum class entityState{
-    IDLE=1, //READY TO WORK, > 15% CHARGE, NO TASK STARTED
-    RUNNING=2,// RUNNING, CHARGE DECREASES PER TICK
-    SEEKING_CHARGER=3,//CHARGE LOW, <15% CHARGE, WILL STOP THE TASK AND STARTS CHARGING, WILL RUN TO THE CHARGING DOCK, WHICH WILL COST TICK AND CHARGE
-    CHARGING=4,//CURRENTLY CHARGING, CHARGE INCREASES BY 10% PER TICK
-    WAITING_FOR_RESOURCE=5,// WAITING TO BE CHARGED OR REPAIRED, 
+    IDLE=1,                //READY TO WORK, > 15% CHARGE, NO TASK STARTED
+    RUNNING=2,             // RUNNING, CHARGE DECREASES PER TICK
+    SEEKING_CHARGER=3,     //CHARGE LOW, <15% CHARGE, WILL STOP THE TASK AND STARTS CHARGING, WILL RUN TO THE CHARGING DOCK, WHICH WILL COST TICK AND CHARGE
+    CHARGING=4,            //CURRENTLY CHARGING, CHARGE INCREASES BY 10% PER TICK
+    UNDER_MAINTAINANCE=5,  // WAITING TO BE REPAIRED, 
     SEEKING_MAINTAINANCE=6,// SEEKS MAINTAINANCE DUE TO REPLACEMENT ISSUE
-    FAULTED=7,// SOMETHING BROKE, GOES TO SEEK MAINTAINANCE 
-    SHUTDOWN=8// TASK DONE, SHUTTING DOWN
+    FAULTED=7,             // SOMETHING BROKE, GOES TO SEEK MAINTAINANCE 
+    SHUTDOWN=8,            // TASK DONE OR LOW CHARGE, SHUTTING DOWN
+    PAUSED=9               // PAUSED, TASK NOT DONE
 };
 
 enum class EntityEvent {
-    EMERGENCY_STOP,
-    SENSOR_OBSTRUCTION,
-    MAINTENANCE_REQUIRED,
-    CLEAR_FAULT
+    EMERGENCY_STOP,     // Operator hits E-Stop; forces entity into FAULTED
+    SENSOR_OBSTRUCTION, // External environment/sensor reports a physical blockage
+    CLEAR_FAULT,        // Operator manually resets an error state after resolving an issue
+    PAUSE_WORK,         // Operator or central system temporarily pauses the current task
+    RESUME_WORK,        // Operator commands the bot to unpause and continue task
+    CANCEL_TASK         // Operator revokes the assigned job, forcing return to IDLE
 };
 
 class Factoryentity{
