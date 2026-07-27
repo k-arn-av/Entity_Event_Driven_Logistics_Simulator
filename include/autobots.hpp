e005490e8d3dfaf206a5981aafb3156f87db7b3a
Arnav Karn <arnavkarna877@gmail.com>
2026-07-27 02:37:23 -0500

    fixed charge getter

--- include/autobots.hpp
+++ include/autobots.hpp
@@ -29,7 +29,7 @@ class autobots: public Factoryentity{
         // and changes the internal attributes like state, is_active, has_active_task, required ticks
 
         //getters and setters
-        double charge()const{
+        double get_charge()const{
             return charge;
         }
 };      
